43396f8f3e267760bdda612854a95c186ee54589
Chang Shin <cshin7374@gmail.com>
2023-06-18 19:26:55 -0500

    commit tree 77b60b33fdb190a49b6f52fa401b4798e259b278

--- db/migrate/20210506153127_create_rides.rb
+++ db/migrate/20210506153127_create_rides.rb
@@ -2,6 +2,9 @@ class CreateRides < ActiveRecord::Migration[6.1]
   def change
     create_table :rides do |t|
 
+      t.taxi_id :integer
+      t.passenger_id :integer
+
       t.timestamps
     end
   end
